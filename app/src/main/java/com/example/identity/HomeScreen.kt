8db8fd869833bf7f50ca20065a4ef3ca627a75bd
Brandon <32801607+Wolf-111@users.noreply.github.com>
2023-02-20 00:18:38 -0600

    refactoring

--- app/src/main/java/com/example/identity/HomeScreen.kt
+++ app/src/main/java/com/example/identity/HomeScreen.kt
@@ -4,11 +4,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.identity.databinding.HomeScreenBinding
 
+interface Identity {
+    val name: String
+    val email: String
+    val phoneNumber: String
+}
+
+var identitiesArray = mutableListOf<Identity>()
+
 class HomeScreen : Fragment() {
     private var _binding: HomeScreenBinding? = null
+
+    private fun generateIdentity(name: String, email: String, phoneNumber: String) {
+        class IdentityImpl(override val name: String, override val email: String, override val phoneNumber: String) : Identity
+        val newIdentity = IdentityImpl(name, email, phoneNumber)
+        identitiesArray.add(newIdentity)
+    }
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
@@ -21,24 +36,20 @@ class HomeScreen : Fragment() {
         return binding.root
     }
 
-    interface Identity {
-        val name: String
-        val email: String
-        val phoneNumber: String
-    }
-
-     private fun generateIdentity(name: String, email: String, phoneNumber: String) {
-         class IdentityImpl(override val name: String, override val email: String, override val phoneNumber: String) : Identity
-         val newIdentity = IdentityImpl(name, email, phoneNumber)
-         
-         binding.identityCardName.text = name
-         binding.identityCardEmail.text = email
-         binding.identityCardPhoneNumber.text = phoneNumber
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val hasNewIdentityBeenCreated = arguments?.getBoolean("identityCreated")
+
+        if(identitiesArray.isNotEmpty()) {
+            val inflater = LayoutInflater.from(context)
+//            for(identity in identitiesArray) {
+//                val cardView = inflater.inflate(R.layout.identity_card, binding.buttonCreateIdentity, false)
+//                cardView.findViewById<TextView>(R.id.identity_card_name).text = identity.name
+//                cardView.findViewById<TextView>(R.id.identity_card_email).text = identity.email
+//                cardView.findViewById<TextView>(R.id.identity_card_phone_number).text = identity.phoneNumber
+//                binding.identityCards.addView(cardView)
+//            }
+        }
 
         if(hasNewIdentityBeenCreated == true){
             val name = arguments?.getString("name")
