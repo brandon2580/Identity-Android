d96cabdd1a30b615f8374e2204f840526a539b17
Brandon <32801607+Wolf-111@users.noreply.github.com>
2023-02-21 16:35:11 -0600

    Removed unnecessary if statement

--- app/src/main/java/com/example/identity/HomeScreen.kt
+++ app/src/main/java/com/example/identity/HomeScreen.kt
@@ -40,26 +40,24 @@ class HomeScreen : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val hasNewIdentityBeenCreated = arguments?.getBoolean("identityCreated")
 
-        if(identitiesArray.isNotEmpty()) {
-            val inflater = LayoutInflater.from(context)
+        val inflater = LayoutInflater.from(context)
 
-            if(hasNewIdentityBeenCreated == true){
-                val name = arguments?.getString("name")
-                val email = arguments?.getString("email")
-                val phoneNumber = arguments?.getString("phoneNumber")
-                if (name != null && email != null && phoneNumber != null) {
-                    generateIdentity(name, email, phoneNumber)
-                }
+        if(hasNewIdentityBeenCreated == true){
+            val name = arguments?.getString("name")
+            val email = arguments?.getString("email")
+            val phoneNumber = arguments?.getString("phoneNumber")
+            if (name != null && email != null && phoneNumber != null) {
+                generateIdentity(name, email, phoneNumber)
             }
+        }
 
-            // Render each Identity "card" individually to display on the user's home screen
-            for(identity in identitiesArray) {
-                val cardView = inflater.inflate(R.layout.identity_cards, binding.homeScreen, false)
-                cardView.findViewById<TextView>(R.id.identity_card_name).text = identity.name
-                cardView.findViewById<TextView>(R.id.identity_card_email).text = identity.email
-                cardView.findViewById<TextView>(R.id.identity_card_phone_number).text = identity.phoneNumber
-                binding.homeScreen.addView(cardView)
-            }
+        // Render each Identity "card" individually to display on the user's home screen
+        for(identity in identitiesArray) {
+            val cardView = inflater.inflate(R.layout.identity_cards, binding.homeScreen, false)
+            cardView.findViewById<TextView>(R.id.identity_card_name).text = identity.name
+            cardView.findViewById<TextView>(R.id.identity_card_email).text = identity.email
+            cardView.findViewById<TextView>(R.id.identity_card_phone_number).text = identity.phoneNumber
+            binding.homeScreen.addView(cardView)
         }
 
         binding.buttonCreateIdentity.setOnClickListener {
