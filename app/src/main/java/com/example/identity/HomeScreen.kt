d2fae4399144a79510c64218842f4222bf365152
Brandon <32801607+Wolf-111@users.noreply.github.com>
2023-02-26 01:31:48 -0600

    Ability to click cards added (wip)

--- app/src/main/java/com/example/identity/HomeScreen.kt
+++ app/src/main/java/com/example/identity/HomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.identity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.identity.databinding.HomeScreenBinding
+import okhttp3.*
+import okio.IOException
 
 interface Identity {
     val name: String
@@ -39,7 +42,6 @@ class HomeScreen : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val hasNewIdentityBeenCreated = arguments?.getBoolean("identityCreated")
-
         val inflater = LayoutInflater.from(context)
 
         if(hasNewIdentityBeenCreated == true){
@@ -51,14 +53,24 @@ class HomeScreen : Fragment() {
             }
         }
 
-        // Render each Identity "card" individually to display on the user's home screen
-        for(identity in identitiesArray) {
+// Render each Identity "card" individually to display on the user's home screen
+        for (i in identitiesArray.indices) {
+            val identity = identitiesArray[i]
             val cardView = inflater.inflate(R.layout.identity_cards, binding.homeScreen, false)
             cardView.findViewById<TextView>(R.id.identity_card_name).text = identity.name
             cardView.findViewById<TextView>(R.id.identity_card_email).text = identity.email
             cardView.findViewById<TextView>(R.id.identity_card_phone_number).text = identity.phoneNumber
             binding.homeScreen.addView(cardView)
+
+            val card = cardView.findViewById<LinearLayout>(R.id.identity_card)
+            card.tag = i // Set the position of the card as its tag
+            card.setOnClickListener {
+                val clickedPosition = it.tag as Int // Get the position of the clicked card from its tag
+                val clickedIdentity = identitiesArray[clickedPosition] // Get the clicked identity from the array
+                println("Clicked on ${clickedIdentity.name}!")
+            }
         }
+
 
         binding.buttonCreateIdentity.setOnClickListener {
             findNavController().navigate(R.id.action_Home_to_CreateIdentity)
